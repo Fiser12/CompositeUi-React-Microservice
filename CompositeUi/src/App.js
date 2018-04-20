3f2d851b40a2b1fc2179d420933ff1efc266e006
Fiser <fiser12@icloud.com>
2018-04-20 21:12:11 +0200

    First commit with the separation of the microservice

---
+++ CompositeUi/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import MenuBar from "./Infrastructure/React/Components/MenuBar";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ CompositeUi/styleguide.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    components: 'src/Infrastructure/React/Components/*.js'
+};
